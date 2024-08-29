ea712a3896a75897c299d3dd828ddecaf9b2a7c4
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-08-29 08:25:10 +0100

    finish working on the code

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'TechPeople Blog';
+export const SITE_DESCRIPTION = 'Articles, stories and tutorials from TechPeople';
+export const HOME_ARTICLES_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 3
